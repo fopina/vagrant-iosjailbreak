0edcdf38af8aa9439f94b9a54280009c66c5b659
Filipe Pina <fopina@gmail.com>
2016-02-26 16:30:04 +0000

    substrate already included in theos

--- scripts/install_theos.sh
+++ scripts/install_theos.sh
@@ -11,15 +11,6 @@ if [ -d $THEOS/.git ]; then
 else
   echo "Installing THEOS"  > /dev/stderr
   git clone --recursive https://github.com/theos/theos $THEOS
-
-  cd /tmp
-  wget --quiet http://apt.saurik.com/debs/mobilesubstrate_0.9.6011_iphoneos-arm.deb
-  mkdir substrate
-  dpkg-deb -x mobilesubstrate_*_iphoneos-arm.deb substrate
-  mv substrate/Library/Frameworks/CydiaSubstrate.framework/CydiaSubstrate $THEOS/lib/libsubstrate.dylib
-  mv substrate/Library/Frameworks/CydiaSubstrate.framework/Headers/CydiaSubstrate.h $THEOS/include/substrate.h
-
-  rm -fr /tmp/substrate /tmp/*.deb
 fi
 
 cat <<EOF > /etc/profile.d/theos.sh
